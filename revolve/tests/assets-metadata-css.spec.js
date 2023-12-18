ab9e5b1acc46855dd02774541e5fe4bf0b5f747c
awh9 <113126029+awh9@users.noreply.github.com>
2023-12-18 00:13:36 -0500

    Closes #32

--- revolve/tests/assets-metadata-css.spec.js
+++ revolve/tests/assets-metadata-css.spec.js
@@ -12,4 +12,15 @@ test("CSS is present", async ({ page }) => {
     const cssLink = await page.$('link[rel="stylesheet"]');
     expect(cssLink).not.toBeNull();
 });
-    
+
+test("Meta description is present", async ({ page }) => {
+    await page.goto(localhost);
+    const metaDescription = await page.$('meta[name="description"]');
+    expect(metaDescription).not.toBeNull();
+});
+
+test("UTF-8 meta tag is present", async ({ page }) => {
+    await page.goto(localhost);
+    const utf8MetaTag = await page.$('meta[charset="UTF-8"]');
+    expect(utf8MetaTag).not.toBeNull();
+});
