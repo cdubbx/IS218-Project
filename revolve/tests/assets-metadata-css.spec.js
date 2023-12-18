c1fafa759cb9ea537bd91fa055a17239a0f880af
awh9 <113126029+awh9@users.noreply.github.com>
2023-12-18 00:09:31 -0500

    Closes #59

--- revolve/tests/assets-metadata-css.spec.js
+++ revolve/tests/assets-metadata-css.spec.js
@@ -5,4 +5,11 @@ const localhost = "http://localhost:3000";
 test("Page title is correct", async ({ page }) => {
     await page.goto(localhost);
     expect(await page.title()).toBe("Revolve");
-    });
+});
+
+test("CSS is present", async ({ page }) => {
+    await page.goto(localhost);
+    const cssLink = await page.$('link[rel="stylesheet"]');
+    expect(cssLink).not.toBeNull();
+});
+    
